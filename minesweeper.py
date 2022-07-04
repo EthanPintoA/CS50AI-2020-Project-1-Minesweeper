295b330c0928c035f3f93dbe43c81e05f99c3ce9
Ethan Pinto <ethanpintoa@gmail.com>
2022-07-04 14:17:32 -0400

    Implement Sentence and MinesweeperAI

--- minesweeper.py
+++ minesweeper.py
@@ -105,27 +105,33 @@ class Sentence():
         """
         Returns the set of all cells in self.cells known to be mines.
         """
-        raise NotImplementedError
+        if len(self.cells) == self.count:
+            return self.cells
+        return set()
 
     def known_safes(self):
         """
         Returns the set of all cells in self.cells known to be safe.
         """
-        raise NotImplementedError
+        if self.count == 0:
+            return self.cells
+        return set()
 
     def mark_mine(self, cell):
         """
         Updates internal knowledge representation given the fact that
         a cell is known to be a mine.
         """
-        raise NotImplementedError
+        if cell in self.cells:
+            self.cells.remove(cell)
+            self.count -= 1
 
     def mark_safe(self, cell):
         """
         Updates internal knowledge representation given the fact that
         a cell is known to be safe.
         """
-        raise NotImplementedError
+        self.cells.discard(cell)
 
 
 class MinesweeperAI():
@@ -182,7 +188,49 @@ class MinesweeperAI():
             5) add any new sentences to the AI's knowledge base
                if they can be inferred from existing knowledge
         """
-        raise NotImplementedError
+        # 1 & 2
+        self.moves_made.add(cell)
+        self.mark_safe(cell)
+
+        # 3
+        neighbors = set()
+
+        for i in range(cell[0] - 1, cell[0] + 2):
+            for j in range(cell[1] - 1, cell[1] + 2):
+                if 0 <= i < self.height and 0 <= j < self.width:
+                    neighbors.add((i, j))
+
+        count -= len(neighbors.intersection(self.mines))
+        neighbors.difference_update(self.mines, self.safes)
+
+        self.knowledge.append(Sentence(neighbors, count))
+
+        # 4
+        for sentence in self.knowledge:
+            for safe in tuple(sentence.known_safes()):
+                self.mark_safe(safe)
+
+            for mine in tuple(sentence.known_mines()):
+                self.mark_mine(mine)
+
+        self.knowledge = [s for s in self.knowledge if s.cells]
+
+        # 5
+        for sentence1, sentence2 in itertools.combinations(self.knowledge, 2):
+            if sentence1 == sentence2:
+                self.knowledge.remove(sentence2)
+            elif sentence1.cells.issuperset(sentence2.cells):
+                new_sentence = Sentence(
+                    sentence1.cells.difference(sentence2.cells),
+                    sentence1.count - sentence2.count,
+                )
+
+                self.knowledge.append(new_sentence)
+
+                try:
+                    self.knowledge.remove(sentence1)
+                except ValueError:
+                    pass
 
     def make_safe_move(self):
         """
@@ -193,7 +241,9 @@ class MinesweeperAI():
         This function may use the knowledge in self.mines, self.safes
         and self.moves_made, but should not modify any of those values.
         """
-        raise NotImplementedError
+        moves = self.safes.difference(self.moves_made)
+
+        return random.choice(tuple(moves)) if moves else None
 
     def make_random_move(self):
         """
@@ -202,4 +252,8 @@ class MinesweeperAI():
             1) have not already been chosen, and
             2) are not known to be mines
         """
-        raise NotImplementedError
+        moves = {(i, j) for i in range(self.height) for j in range(self.width)}
+
+        moves.difference_update(self.moves_made, self.mines)
+
+        return random.choice(tuple(moves)) if moves else None
